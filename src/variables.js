2c431803fb7030ff538757b31435a3974bd9ade9
Jasdeep <jhbsk@users.noreply.github.com>
2017-02-23 12:03:39 +0000

    Added variables and nesting into CSS

--- src/variables.js
+++ src/variables.js
@@ -1,6 +1,10 @@
 const globalVariables = {
-    progressBlue: '#007BCD',
-    progressGrey: '#ECECEC'
+    titleText: '#101010',
+    salesBlue: '#007BCD',
+    salesBlueText: '#E7F2FA',
+    salesGrey: '#ECECEC',
+    lineGraphPredictionGrey: '#D7D7D7',
+    statGreyText: '#646464'
 }
 
 module.exports = globalVariables
